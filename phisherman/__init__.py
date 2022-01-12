2094038252158a0afefb0c1208d35c7bee351634
1olipop <mohammedrayyan112233@gmail.com>
2022-01-12 13:26:20 +0530

    Fix Lint

--- phisherman/__init__.py
+++ phisherman/__init__.py
@@ -3,7 +3,7 @@ from .errors import InvalidRequest
 
 __version__ = "0.1.0"
 __author__ = "Vedrecide"
-__copyright__ = f"Copyright (c) Vedrecide"
+__copyright__ = "Copyright (c) Vedrecide"
 __license__ = "MIT"
 __description__ = "Asynchronous Python API Wrapper for phisherman.gg"
 __github__ = "https://github.com/QristaLabs/phisherman.py"
