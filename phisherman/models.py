2834407c9cb352a7efdf79883c4584232cb144d8
janaSunrise <warriordefenderz@gmail.com>
2022-03-22 18:13:09 +0530

    Fix typing issue.

--- phisherman/models.py
+++ phisherman/models.py
@@ -1,17 +1,17 @@
+import typing as t
 from dataclasses import dataclass
-from typing import Literal
 
 from .utils.metaclasses import FromDictMeta
 
 
 @dataclass
 class DomainCheck(metaclass=FromDictMeta):
-    classification: Literal["malicious", "suspicious", "unknown", "safe"]
+    classification: t.Literal["malicious", "suspicious", "safe", "unknown"]
     verified_phish: bool
 
     @classmethod
     def from_dict(cls, data: dict) -> "DomainCheck":
         return cls(
-            classification=data.get("classification", ""),
+            classification=data.get("classification", "unknown"),
             verified_phish=data.get("verifiedPhish", False),
         )
